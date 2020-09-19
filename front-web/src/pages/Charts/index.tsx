28fb9e858c4ca3c38ced417cba140c43ab89471f
Filipe dos Santos <fhilips.souza@gmail.com>
2020-09-19 17:48:09 -0300

    Adicionando páginahome no React Native

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Filters from '../../components/Filters';
 import './styles.css';
 import Chart from 'react-apexcharts';
-import {barOptions, pieOptions} from './chart-options';
+import { barOptions, pieOptions } from './chart-options';
 import axios from "axios";
-import { buildBarSeries, getGenderChartData, getPlatformChartData} from './helpers'
+import { buildBarSeries, getGenderChartData, getPlatformChartData } from './helpers'
 
 type PieChartData = {
     labels: string[];
@@ -29,10 +29,10 @@ const Charts = () => {
     const [genderData, setGenderData] = useState<PieChartData>(inicialPieData);
 
     useEffect(() => {
-        async function getData(){
+        async function getData() {
             const recordsResponse = await axios.get(`${BASE_URL}/records`);
             const gamesResponse = await axios.get(`${BASE_URL}/games`);
-           
+
             const barData = buildBarSeries(gamesResponse.data, recordsResponse.data.content);
             setBarChartData(barData);
 
@@ -49,42 +49,41 @@ const Charts = () => {
         <div className="page-container">
             <Filters link="/records" linkText="VER TABELA" />
             <div className="chart-container">
-            <div className="top-related">
-                <h1 className="top-related-title">
-                    Jogos mais votados
-                </h1>
-                <div className="games-container">
-                    <Chart 
-                    options={barOptions} 
-                    type="bar"
-                    width="900"
-                    height="650"          
-                    series={[{data: barChartData }]}          
-                    />
-                </div>
+                <div className="top-related">
+                    <h1 className="top-related-title">
+                        Jogos mais votados
+                    </h1>
+                    <div className="games-container">
+                        <Chart
+                            options={barOptions}
+                            type="bar"
+                            width="900"
+                            height="650"
+                            series={[{ data: barChartData }]}
+                        />
+                    </div>
                 </div>
                 <div className="charts">
                     <div className="platform-chart">
                         <h2 className="chart-title">Plataformas</h2>
-                        <Chart options={{ ...pieOptions, labels: platChartData?.labels}}
-                                type="donut"
-                                series={platChartData?.series}
-                                width="350"
-                         />
+                        <Chart options={{ ...pieOptions, labels: platChartData?.labels }}
+                            type="donut"
+                            series={platChartData?.series}
+                            width="350"
+                        />
                     </div>
                     <div className="gender-chart">
                         <h2 className="chart-title">Gêneros</h2>
-                        <Chart options={{ ...pieOptions, labels: genderData?.labels}}
-                                type="donut"
-                                series={genderData?.series}
-                                width="350"
-                         />
+                        <Chart options={{ ...pieOptions, labels: genderData?.labels }}
+                            type="donut"
+                            series={genderData?.series}
+                            width="350"
+                        />
                     </div>
                 </div>
-
             </div>
         </div>
-        
+
     )
 };
 
